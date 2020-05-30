9ff4cb801a4720a866da64866f6261af5e60cafc
yuki sawabe <sawabe0v0@gmail.com>
2020-05-30 09:48:00 +0900

    add getter to Quiz.js

--- src/models/Quiz.js
+++ src/models/Quiz.js
@@ -4,6 +4,14 @@ class Quiz {
     this._correctAnswer = correctAnswer;
     this._incorrectAnswer = [...incorrectAnswers];
   }
+
+  get question() {
+    return this._question;
+  }
+
+  get correctAnswer() {
+    return this._correctAnswer;
+  }
 }
 
 export default Quiz;
